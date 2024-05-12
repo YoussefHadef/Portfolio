434ae8a3d0f8ac652bbe6c06ad3491a8c108e3ed
Youssef Hadef <125701456+YoussefHadef@users.noreply.github.com>
2024-05-12 09:25:58 -0700

    commit

--- Portfolio.js
+++ Portfolio.js
@@ -26,4 +26,4 @@ response.writeHead(200, {'Content-Type' : 'text/plain'});
 response.end('Bonjour tot le monde\n');
 }).écouter(3000);
 // The console imprimera the message
-console.log('Server running at http://127.0.0.1:3000/');
+console.log('Server running at  http://localhost:80');
